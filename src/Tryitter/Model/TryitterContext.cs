a369677a646afefd424d9dd536768ba720e24c75
Raphael Martins <raphael.almeida.martins@gmail.com>
2023-03-04 22:52:11 -0300

    :rocket: primeiro commit. Setup do projeto

---
+++ src/Tryitter/Model/TryitterContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Tryitter.Model
+{
+  public partial class TryitterContext : DbContext
+  {
+    private readonly IConfiguration _config;
+
+    public TryitterContext(IConfiguration config)
+    {
+      _config = config;
+    }
+
+    public TryitterContext(DbContextOptions<TryitterContext> options, IConfiguration config)
+        : base(options)
+    {
+      _config = config;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      if (!optionsBuilder.IsConfigured)
+      {
+        optionsBuilder.UseSqlServer(_config.GetConnectionString("Tryitter_DB"));
+      }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+  }
+}
