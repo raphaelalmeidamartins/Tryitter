d79cfb8f0646bc978d618bc375edd631d1e025ed
Raphael Martins <raphael.almeida.martins@gmail.com>
2023-03-04 23:51:50 -0300

    :wrench: chore: adiciona constraints aos campos da tabela Images

--- src/Tryitter/Models/Image.cs
+++ src/Tryitter/Models/Image.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Tryitter.Models;
 
 public class Image
 {
   public int ImageId { get; private set; }
+
+  [Required]
+  [MaxLength(255)]
   public string FileName { get; set; } = default!;
+
+  [Required]
+  [MaxLength(100)]
   public string ContentType { get; set; } = default!;
+
+  [Required]
+  [MaxLength(1048576)]
   public byte[] Data { get; set; } = default!;
 }
