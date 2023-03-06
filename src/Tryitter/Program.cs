ef40c0ca5021aa12c082b1aeeebe3cf17eae4fae
Raphael Martins <raphael.almeida.martins@gmail.com>
2023-03-06 08:34:33 -0300

    :sparkles: feat: adiciona Seeders

--- src/Tryitter/Program.cs
+++ src/Tryitter/Program.cs
@@ -51,6 +51,12 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+var context = services.GetRequiredService<TryitterContext>();
+await context.SeedAsync();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
--- src/Tryitter/Repositories/TryitterContext.cs
+++ src/Tryitter/Repositories/TryitterContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Tryitter.Models;
+using Tryitter.Utils;
 
 namespace Tryitter.Repositories;
 
@@ -53,4 +54,52 @@ public partial class TryitterContext : DbContext, ITryitterContext
   }
 
   partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+  public async Task SeedAsync()
+  {
+    using var transaction = await Database.BeginTransactionAsync();
+
+    try
+    {
+      if (!Modules.Any())
+      {
+        Module fundamentos = new() { Name = "Fundamentos" };
+        Module frontEnd = new() { Name = "FrontEnd" };
+        Module BackEnd = new() { Name = "BackEnd" };
+        Module computerScience = new() { Name = "Ciência da Computação" };
+        Module beyond = new() { Name = "Beyond" };
+
+        Modules.AddRange(fundamentos, frontEnd, BackEnd, computerScience, beyond);
+        await SaveChangesAsync();
+      }
+
+      if (!Users.Any())
+      {
+        var adminModule = await Modules.SingleAsync(m => m.Name == "Beyond");
+
+        User admin = new()
+        {
+          Username = "admin",
+          Email = "admin@admin.com",
+          PasswordHash = await PasswordHasher.HashPasswordAsync("admin"),
+          Bio = "The boss",
+          Status = "Bossing around",
+          ModuleId = adminModule.ModuleId,
+          Module = adminModule,
+          IsAdmin = true,
+        };
+
+        Users.Add(admin);
+        await SaveChangesAsync();
+      }
+
+      await transaction.CommitAsync();
+    }
+    catch (Exception ex)
+    {
+      await transaction.RollbackAsync();
+      throw new Exception("Error seeding the database", ex);
+    }
+  }
+
 }
