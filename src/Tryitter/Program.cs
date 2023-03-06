6828ffb8f47a18b8c906f098a6925280dde5dcd4
Raphael Martins <raphael.almeida.martins@gmail.com>
2023-03-06 06:55:48 -0300

    :wrench: chore: injeta dependências e adiciona services

--- src/Tryitter/Program.cs
+++ src/Tryitter/Program.cs
@@ -1,23 +1,66 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Tryitter.Repositories;
+using Tryitter.Utils;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<TryitterContext>();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<ITryitterContext, TryitterContext>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserFollowerRepository, UserFollowerRepository>();
+builder.Services.AddScoped<IPostRepository, PostRepository>();
+builder.Services.AddScoped<IModuleRepository, ModuleRepository>();
+builder.Services.AddScoped<ITokenGenerator, TokenGenerator>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(options =>
+{
+  options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+  options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+  options.RequireHttpsMetadata = false;
+  options.SaveToken = true;
+  options.TokenValidationParameters = new TokenValidationParameters()
+  {
+    ValidateIssuer = false,
+    ValidateIssuerSigningKey = true,
+    ValidateAudience = false,
+    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("2d74025e7bcf058897d8daaa99ae99b5"))
+  };
+});
+builder.Services.AddAuthorization(options =>
+{
+  options.AddPolicy("UserPolicy", policy =>
+    policy.RequireRole("User"));
+  options.AddPolicy("AdminPolicy", policy =>
+    policy.RequireRole("Admin"));
+});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
