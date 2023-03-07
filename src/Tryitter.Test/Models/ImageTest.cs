65ca1c1b056d9dd72f32ce1e64a175003ba208f1
Raphael Martins <raphael.almeida.martins@gmail.com>
2023-03-06 23:55:48 -0300

    :test_tube: test: Image model

---
+++ src/Tryitter.Test/Models/ImageTest.cs
@@ -0,0 +1,138 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using FluentAssertions;
+using Xunit;
+using Tryitter.Models;
+
+namespace Tryitter.Test.Models
+{
+  public class ImageTests
+  {
+    [Fact]
+    public void Image_ShouldHaveProperties()
+    {
+      // Arrange
+      var image = new Image();
+
+      // Act
+
+      // Assert
+      image.Should().NotBeNull();
+      image.Should().BeAssignableTo<Image>();
+      image.FileName.Should().BeNull();
+      image.ContentType.Should().BeNull();
+      image.Data.Should().BeNull();
+      image.Users.Should().NotBeNull().And.BeEmpty();
+      image.Posts.Should().NotBeNull().And.BeEmpty();
+    }
+
+    [Fact]
+    public void Image_FileName_ShouldHaveRequiredAttribute()
+    {
+      // Arrange
+      var property = typeof(Image).GetProperty(nameof(Image.FileName));
+
+      // Act
+      var attribute = property?.GetCustomAttributes(typeof(RequiredAttribute), true);
+
+      // Assert
+      attribute.Should().NotBeNull().And.HaveCount(1);
+    }
+
+    [Fact]
+    public void Image_FileName_ShouldHaveMaxLengthAttribute()
+    {
+      // Arrange
+      var property = typeof(Image).GetProperty(nameof(Image.FileName));
+
+      // Act
+      var attribute = property?.GetCustomAttributes(typeof(MaxLengthAttribute), true);
+
+      // Assert
+      attribute.Should().NotBeNull().And.HaveCount(1);
+      attribute.Should().BeOfType<MaxLengthAttribute[]>()
+        .Which[0].Length.Should().Be(255);
+
+    }
+
+    [Fact]
+    public void Image_ContentType_ShouldHaveRequiredAttribute()
+    {
+      // Arrange
+      var property = typeof(Image).GetProperty(nameof(Image.ContentType));
+
+      // Act
+      var attribute = property?.GetCustomAttributes(typeof(RequiredAttribute), true);
+
+      // Assert
+      attribute.Should().NotBeNull().And.HaveCount(1);
+    }
+
+    [Fact]
+    public void Image_ContentType_ShouldHaveMaxLengthAttribute()
+    {
+      // Arrange
+      var property = typeof(Image).GetProperty(nameof(Image.ContentType));
+
+      // Act
+      var attribute = property?.GetCustomAttributes(typeof(MaxLengthAttribute), true);
+
+      // Assert
+      attribute.Should().NotBeNull().And.HaveCount(1);
+      attribute.Should().BeOfType<MaxLengthAttribute[]>()
+          .Which[0].Length.Should().Be(100);
+    }
+
+    [Fact]
+    public void Image_Data_ShouldHaveRequiredAttribute()
+    {
+      // Arrange
+      var property = typeof(Image).GetProperty(nameof(Image.Data));
+
+      // Act
+      var attribute = property?.GetCustomAttributes(typeof(RequiredAttribute), true);
+
+      // Assert
+      attribute.Should().NotBeNull().And.HaveCount(1);
+    }
+
+    [Fact]
+    public void Image_Data_ShouldHaveMaxLengthAttribute()
+    {
+      // Arrange
+      var property = typeof(Image).GetProperty(nameof(Image.Data));
+
+      // Act
+      var attribute = property?.GetCustomAttributes(typeof(MaxLengthAttribute), true);
+
+      // Assert
+      attribute.Should().NotBeNull().And.HaveCount(1);
+      attribute.Should().BeOfType<MaxLengthAttribute[]>()
+          .Which[0].Length.Should().Be(1048576);
+    }
+
+    [Fact]
+    public void Image_Users_ShouldBeEmptyCollection()
+    {
+      // Arrange
+      var image = new Image();
+
+      // Act
+
+      // Assert
+      image.Users.Should().NotBeNull().And.BeEmpty();
+    }
+
+    [Fact]
+    public void Image_Posts_ShouldBeEmptyCollection()
+    {
+      // Arrange
+      var image = new Image();
+
+      // Act
+
+      // Assert
+      image.Posts.Should().NotBeNull().And.BeEmpty();
+    }
+  }
+}
